6ed8ccb433d7aaef1b91c554f2163185c5c6a577
anof1r <denchik.anofriev@gmail.com>
2026-08-13 15:48:11 +0300

    empty url path fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/display', pathMatch: 'full' },
   {
     path: 'dm',
     loadComponent: () =>
       import('./features/dm-control/dm-control.component').then((m) => m.DmControlComponent),
   },
   {
-    path: 'display',
-    loadComponent: () =>
-      import('./features/display/display.component').then((m) => m.DisplayComponent),
-  },
-  {
     path: 'player',
     loadComponent: () =>
       import('./features/player/player.component').then((m) => m.PlayerComponent),
   },
-  { path: '', redirectTo: '/dm', pathMatch: 'full' },
 ];
